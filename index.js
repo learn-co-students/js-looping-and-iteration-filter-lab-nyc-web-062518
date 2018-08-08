e80305a00a5c14624db07e4d20a129b70870e097
Jack Yeung <jyeung.se@gmail.com>
2018-08-07 22:28:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
 // Code your solution in this file
 function findMatching (drivers, name) {
-  const newDriver = [];
-
-  for (const driver of drivers) {
-    if (driver.toLowerCase() === name.toLowerCase()) {
-      newDriver.push(driver);
-    }
-  }
-  return newDriver;
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
 }
 
-
 function fuzzyMatch(drivers, name) {
-  const newDriver = [];
-
-  for (const driver of drivers) {
-    if (driver[0] === name[0]) {
-      newDriver.push(driver);
-    }
-  }
-  return newDriver;
+  return drivers.filter(function(driver) {
+    return driver[0] === name[0]
+  });
 }
 
 
 function matchName(drivers, name) {
-  const newDriver = [];
-
-  for (const driver of drivers) {
-    if (driver.name === name) {
-      newDriver.push(driver);
-    }
-  }
-  return newDriver;
+  return drivers.filter(function(driver) {
+    return (driver.name === name)
+  });
 }
